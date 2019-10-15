c0204c547f69c8e078ff1af1d9a60f6021ae160c
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2019-10-15 00:33:01 -0500

    linked FontAwesome to styling

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -9,10 +9,12 @@ export default class Header extends Component {
     };
   }
   render() {
-    return (<section className="Listings">Listings</section>
-    <section className="search-area">
-      <input type="text" name="search" />
-    </section>
+    return (
+    <section className="Listings">
+      <section className="search-area">
+        <input type="text" name="search" />
+      </section>
+
     <section className="sortby-area">
       <div>390 results found</div>
       <div className="sort-options">
@@ -24,11 +26,28 @@ export default class Header extends Component {
         <i className="fas fa-list"></i>
         <i className="fas fa-th"></i>
       </div>
+    </div>
+  </section>
+
+    <section className="listings-results">
+      <div className="listing">
+        <div className="listing-image">
+          <span className="Address">Address</span>
+          <div className="details">
+            <div className="user-image"></div>
+            <div className="user-details">
+              <span className="user-name">Nina Smith</span>
+              <span className="post-date">05/05/2019</span>
+            </div>
+            <div className="listing-details">
+              <div className="floor-space"><i class="far fa-square"></i>1000 ft&sup2;</div>
+              <i className="fas fa-bed"></i>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
-
-    <section className="listings-results"></section>
     <section className="pagination"></section>
-    
-    )}
+    );
+    }
 }
